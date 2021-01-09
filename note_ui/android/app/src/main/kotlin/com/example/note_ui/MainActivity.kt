c7c9b30931c2be73b1cfa361b7a9bd4a0083e5af
josepaulodelacruz <whoseepowlu@gmail.com>
2021-01-09 15:20:45 +0800

    fix: unchange package name

--- note_ui/android/app/src/main/kotlin/com/example/note_ui/MainActivity.kt
+++ note_ui/android/app/src/main/kotlin/com/example/note_ui/MainActivity.kt
@@ -1,6 +1,8 @@
-package com.example.note_ui
+package com.jeyps.lazy_notes
 
+import android.os.Bundle
 import io.flutter.embedding.android.FlutterActivity
+import io.flutter.plugins.GeneratedPluginRegistrant
 
 class MainActivity: FlutterActivity() {
 }
